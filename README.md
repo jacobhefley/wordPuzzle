6240a7f56664883a04d967defda95c7aa3b4d7ca
Jacob Hefley <jacobhefley3@gmail.com>
2016-04-22 13:49:21 -0700

    meets requirements

---
+++ README.md
@@ -0,0 +1,31 @@
+# _WordPuzzle_
+
+#### _04/22/2016_
+
+#### By Jacob Hefley_
+
+## Description
+
+_A Basic Java Enable website, running ona local server powered by Spark and Gradle, It takes in an input and converts all the vowels (upper and lower case) and turns them into '-' chars for the user to guess._
+
+
+## Setup/Installation Requirements
+
+* _git clone the repo_
+
+
+## Future Improvements
+
+_Add another form after input that will check the string._
+
+
+
+## Technologies Used
+
+_JavaScript, HTML, CSS, Bootstrap, Java, Gradle, Spark, Fluentlenium_
+
+### License
+
+*MIT license*
+
+Copyright (c) 2016
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest extends FluentTest {
 
   @ClassRule
   public static ServerRule server = new ServerRule();
-  
+
   @Test
   public void makePuzzle_returnsForA() {
     App yourApp = new App();
@@ -31,50 +31,17 @@ public class AppTest extends FluentTest {
     String expected = "-BC";
     assertEquals(expected, yourApp.makePuzzle("ABC"));
   }
-  // @Test
-  // public void makePuzzle_returnsForA() {
-  //   Scrabble yourApp = new Scrabble();
-  //   String expected = "----------";
-  //   assertEquals(expected, yourApp.makePuzzle("The quick brown fox jumps over the lazy dog"));
-  // }
-  // @Test
-  // public void calculateScore_returnsScoreFor3pointers() {
-  //   Scrabble yourApp = new Scrabble();
-  //   Integer expected = 12;
-  //   assertEquals(expected, yourApp.calculateScore("bcpm"));
-  // }
-  //
-  // @Test
-  // public void calculateScore_returnsScoreFor4pointers() {
-  //   Scrabble yourApp = new Scrabble();
-  //   Integer expected = 20;
-  //   assertEquals(expected, yourApp.calculateScore("fhvwy"));
-  // }
-  //
-  // @Test
-  // public void calculateScore_returnsScoreFor5pointers() {
-  //   Scrabble yourApp = new Scrabble();
-  //   Integer expected = 5;
-  //   assertEquals(expected, yourApp.calculateScore("k"));
-  // }
-  //
-  // @Test
-  // public void calculateScore_returnsScoreFor8pointers() {
-  //   Scrabble yourApp = new Scrabble();
-  //   Integer expected = 16;
-  //   assertEquals(expected, yourApp.calculateScore("jx"));
-  // }
-  //
-  // @Test
-  // public void calculateScore_returnsScoreFor10pointers() {
-  //   Scrabble yourApp = new Scrabble();
-  //   Integer expected = 20;
-  //   assertEquals(expected, yourApp.calculateScore("zq"));
-  // }
-  // @Test
-  // public void calculateScore_returnsScoreForAll() {
-  //   Scrabble yourApp = new Scrabble();
-  //   Integer expected = 87;
-  //   assertEquals(expected, yourApp.calculateScore("abcdefghijklmnopqrstuvwxyz"));
-  // }
+  @Test
+  public void makePuzzle_returnsForFox() {
+    App yourApp = new App();
+    String expected = "Th- q--ck br-wn f-x j-mps -v-r th- l-zy d-g";
+    assertEquals(expected, yourApp.makePuzzle("The quick brown fox jumps over the lazy dog"));
+  }
+  @Test
+    public void rootTestandFoxEntry() {
+      goTo("http://localhost:4567/");
+      fill("#sentence").with("The quick brown fox jumps over the lazy dog");
+      submit(".btn");
+      assertThat(pageSource()).contains("Th- q--ck br-wn f-x j-mps -v-r th- l-zy d-g");
+  }
 }
